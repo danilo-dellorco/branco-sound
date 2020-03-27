400e90662943d5283091e88d65a14f1a2138b18c
danilo-dellorco <danilodellorcolp@gmail.com>
2020-03-27 11:00:42 +0100

    try listview

--- app/src/main/java/it/danilodellorco/brancosoundboard/MainActivity.java
+++ app/src/main/java/it/danilodellorco/brancosoundboard/MainActivity.java
@@ -9,7 +9,10 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,64 +20,44 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public class holder implements View.OnClickListener{
-        Button btnPlay;
-        Button btnShare;
+    public class holder{
+        ListView listView;
         MediaPlayer mp;
 
         public holder(){
-            btnPlay = findViewById(R.id.btnPlay);
-            btnShare = findViewById(R.id.btnShare);
-            mp = MediaPlayer.create(getApplicationContext(), R.raw.battisti);
-            btnPlay.setOnClickListener(this);
-            btnShare.setOnClickListener(this);
-        }
+            mp = MediaPlayer.create(getApplicationContext(),R.raw.Incoerenza);
+            listView = findViewById(R.id.listView);
+            /// Getting list of Strings from your resource
+            String[] testArray = getResources().getStringArray(R.array.Luca);
+            List<String> testList = Arrays.asList(testArray);
 
-        private void CopyRAWtoSDCard(int id, String path) throws IOException {
-            File mydir = new File(Environment.getExternalStorageDirectory() + "/branco/");
-            if(!mydir.exists())
-                mydir.mkdirs();
-            else
-                Log.d("error", "dir. already exists");
-            InputStream in = getResources().openRawResource(id);
-            FileOutputStream out = new FileOutputStream(path);
-            byte[] buff = new byte[1024];
-            int read = 0;
-            try {
-                while ((read = in.read(buff)) > 0) {
-                    out.write(buff, 0, read);
-                }
-            } finally {
-                in.close();
-                out.close();
-            }
-        }
+            // Instanciating Adapter
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
+                    android.R.layout.simple_list_item_1, testList);
 
-        @Override
-        public void onClick(View v) {
-            if (v.getId() == R.id.btnPlay){
-                mp.start();
-            }
-            if (v.getId() == R.id.btnShare){
-                String path = Environment.getExternalStorageDirectory() + "/branco";
-                try {
-                    CopyRAWtoSDCard(R.raw.battisti,path + File.separator + "battisti.mp3");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            // setting adapter on listview
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter, final View componente, int pos, long id){
+                        // recupero il titolo memorizzato nella riga tramite l'ArrayAdapter
+                        final String titoloriga = (String) adapter.getItemAtPosition(pos);
+                        Log.d("List", "Ho cliccato sull'elemento con titolo " + titoloriga);
+                        if (titoloriga == "1"){
 
-                Uri uri = Uri.parse(path + File.separator + "battisti.mp3");
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                //String mimeType = getContentResolver().getType(uri);
-                sharingIntent.setType("audio/mp3");
-                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                            mp.start();
+                        }
+                    }
+                });
             }
-        }
+
+
     }
 
 
